3d1c5233a66428de7f4279192f78d85c1c38c38c
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-21 04:07:13 +0530

    renamed ui package to @repo/ui

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,8 @@
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["ui"],
+	reactStrictMode: true,
+	transpilePackages: ['@repo/ui'],
 };
 
 module.exports = withVanillaExtract(nextConfig);
